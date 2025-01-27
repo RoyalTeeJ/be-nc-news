35b87553f8ee9f4cb1386e2b118ae145717f5168
RoyalTeeJ <tiagostar13@gmail.com>
2025-01-27 16:21:53 +0000

    GET /api/Topics and error handle

--- app.js
+++ app.js
@@ -1,9 +1,17 @@
 const express = require('express');
 const app = express();
 const endpoints = require('./endpoints.json');
+const {getTopics} = require('./controllers')
+
+app.get('/api/topics', getTopics)
 
 app.get('/api', (request, response)=>{
-    response.status(200).json({endpoints})
+
+    response.status(200).send({endpoints})
+})
+
+app.all('*', (request,response)=>{
+    response.status(404).send({error: 'Endpoint not found'})
 })
 
 module.exports = app
