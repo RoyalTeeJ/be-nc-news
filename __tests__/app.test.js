1aaadc8397bb339f161dbcc7077df89866128f3d
RoyalTeeJ <tiagostar13@gmail.com>
2025-01-27 12:52:55 +0000

    01/02 - test to respond with each endpoint from endpoint.json file

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -1,7 +1,17 @@
 const endpointsJson = require("../endpoints.json");
-/* Set up your test imports here */
+const app = require('../app.js')
+const request = require('supertest')
+const db = require('../db/connection.js');
+const seed = require('../db/seeds/seed'); 
+const testData = require('../db/data/test-data/index.js')
 
-/* Set up your beforeEach & afterAll functions here */
+beforeEach(() => {
+  return seed(testData);
+});
+
+afterAll(() => {
+  return db.end();
+});
 
 describe("GET /api", () => {
   test("200: Responds with an object detailing the documentation for each endpoint", () => {
