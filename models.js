11888f46c36f57ba901053283954b7213075467d
RoyalTeeJ <tiagostar13@gmail.com>
2025-01-28 10:19:32 +0000

    fetch articles table by given article_id

--- models.js
+++ models.js
@@ -1,13 +1,30 @@
-const db = require('./db/connection.js');
+const db = require("./db/connection.js");
 
+function fetchTopics() {
+  let SQLString = `SELECT * FROM topics`;
+  return db.query(SQLString).then((response) => {
+    return response.rows;
+  });
+}
 
-function fetchTopics(){
-const SQLString = `SELECT * FROM topics`
-return db.query(SQLString).then((response)=>{
-return response.rows
-})
+function fetchArticleID(article_id) {
+  let SQLString = `SELECT * FROM articles`;
+  const args = [];
+
+  if (article_id) {
+    SQLString += " WHERE article_id = $1";
+    args.push(article_id);
+  }
+
+  return db.query(SQLString, args).then((response) => {
+    if (response.rows.length === 0) {
+      return Promise.reject({ message: "Not Found", status: 404 });
+    }
+    return response.rows[0];
+  });
 }
 
 module.exports = {
-    fetchTopics
-}
+  fetchTopics,
+  fetchArticleID,
+};
