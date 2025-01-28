3e5c6158a6685cf1a9e1286d54820cac7004e2cf
RoyalTeeJ <tiagostar13@gmail.com>
2025-01-28 15:07:07 +0000

    update tests with length property where most suitable, add a test to api/articles to make sure there is no body property, finish test that checks api/articles created_at key is in descending order and add tests to GET api/articles/:articles_id/comments including error handling tests

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -38,8 +38,9 @@ describe("GET /api/topics", () => {
       .get("/api/topics")
       .expect(200)
       .then(({ body }) => {
-        expect(Array.isArray(body)).toBe(true);
-        body.forEach((topic) => {
+        expect(body.topics.length).toBe(3);
+        expect(Array.isArray(body.topics)).toBe(true);
+        body.topics.forEach((topic) => {
           expect(topic).toHaveProperty("slug");
           expect(topic).toHaveProperty("description");
         });
@@ -52,15 +53,16 @@ describe("GET /api/articles/:article_id", () => {
     return request(app)
       .get(`/api/articles/1`)
       .expect(200)
-      .then((response) => {
-        expect(response.body).toHaveProperty("article_id");
-        expect(response.body).toHaveProperty("title");
-        expect(response.body).toHaveProperty("author");
-        expect(response.body).toHaveProperty("body");
-        expect(response.body).toHaveProperty("topic");
-        expect(response.body).toHaveProperty("created_at");
-        expect(response.body).toHaveProperty("votes");
-        expect(response.body).toHaveProperty("article_img_url");
+      .then(({ body }) => {
+        expect(body.article.article_id).toBe(1);
+        expect(body.article).toHaveProperty("article_id");
+        expect(body.article).toHaveProperty("title");
+        expect(body.article).toHaveProperty("author");
+        expect(body.article).toHaveProperty("body");
+        expect(body.article).toHaveProperty("topic");
+        expect(body.article).toHaveProperty("created_at");
+        expect(body.article).toHaveProperty("votes");
+        expect(body.article).toHaveProperty("article_img_url");
       });
   });
 
@@ -84,13 +86,15 @@ describe("GET /api/articles/:article_id", () => {
 });
 
 describe("GET /api/articles", () => {
-  test.only("GET: 200 should return an array of articles with the correct properties", () => {
+  test("GET: 200 should return an array of articles with the correct properties", () => {
     return request(app)
       .get("/api/articles")
       .expect(200)
       .then(({ body }) => {
-        expect(Array.isArray(body)).toBe(true);
-        body.forEach((article) => {
+        expect(body.article.length).toBe(5);
+        expect(Array.isArray(body.article)).toBe(true);
+        body.article.forEach((article) => {
+          expect(typeof article.article_id).toBe("number");
           expect(article).toHaveProperty("article_id");
           expect(article).toHaveProperty("author");
           expect(article).toHaveProperty("title");
@@ -103,5 +107,77 @@ describe("GET /api/articles", () => {
       });
   });
 
-  test("GET: 200 should return articles sorted by created_at in descending order", () => {});
+  test("GET: 200 articles should not return with a body property", () => {
+    return request(app)
+      .get("/api/articles")
+      .expect(200)
+      .then(({ body }) => {
+        body.article.forEach((article) => {
+          expect(article.body).toBeUndefined;
+        });
+      });
+  });
+
+  test("GET: 200 should return articles sorted by created_at in descending order", () => {
+    return request(app)
+      .get("/api/articles")
+      .expect(200)
+      .then(({ body }) => {
+        expect(body.article).toBeSorted({
+          key: "created_at",
+          descending: true,
+        });
+      });
+  });
+});
+
+describe("GET /api/articles/:article_id/comments", () => {
+  test("GET: 200 should return an array of comments for the given article_id", () => {
+    return request(app)
+      .get("/api/articles/1/comments")
+      .expect(200)
+      .then(({ body }) => {
+        expect(body.comments.length).toBe(11);
+        expect(Array.isArray(body.comments)).toBe(true);
+        body.comments.forEach((comment) => {
+          expect(comment.article_id).toBe(1);
+          expect(comment).toHaveProperty("comment_id");
+          expect(comment).toHaveProperty("votes");
+          expect(comment).toHaveProperty("created_at");
+          expect(comment).toHaveProperty("author");
+          expect(comment).toHaveProperty("body");
+          expect(comment).toHaveProperty("article_id");
+        });
+      });
+  });
+
+  test("GET: 200 should return comments sorted by created_at in descending order", () => {
+    return request(app)
+      .get("/api/articles/1/comments")
+      .expect(200)
+      .then(({ body }) => {
+        expect(body.comments).toBeSorted({
+          key: "created_at",
+          descending: true,
+        });
+      });
+  });
+
+  test("GET: 404 should return 404 if no comments exist for a given article", () => {
+    return request(app)
+      .get("/api/articles/9999/comments")
+      .expect(404)
+      .then((response) => {
+        expect(response.body.message).toBe("Not Found");
+      });
+  });
+
+  test("GET: 400 should return 400 if article_id is invalid non-numeric", () => {
+    return request(app)
+      .get("/api/articles/randomWord/comments")
+      .expect(400)
+      .then((response) => {
+        expect(response.body.message).toBe("Bad Request");
+      });
+  });
 });
