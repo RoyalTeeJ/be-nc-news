3ccf573f86f7590ef8ec0dc21b70e74c7ecd47eb
RoyalTeeJ <tiagostar13@gmail.com>
2025-01-28 11:22:16 +0000

    return articles to the controller

--- models.js
+++ models.js
@@ -24,7 +24,15 @@ function fetchArticleID(article_id) {
   });
 }
 
+function fetchArticles() {
+  let SQLString = `SELECT articles.article_id, articles.author, articles.title, articles.topic, articles.created_at, articles.votes, articles.article_img_url, COUNT(comments.comment_id) AS comment_count FROM articles JOIN comments ON articles.article_id = comments.article_id GROUP BY articles.article_id ORDER BY articles.created_at DESC;`;
+  return db.query(SQLString).then((response) => {
+    return response.rows;
+  });
+}
+
 module.exports = {
   fetchTopics,
   fetchArticleID,
+  fetchArticles,
 };
