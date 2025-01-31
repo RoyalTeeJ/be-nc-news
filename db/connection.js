fb1de82a5f400f05558e5cfef2273ad22ccd3b54
RoyalTeeJ <tiagostar13@gmail.com>
2025-01-31 10:36:27 +0000

    updated productions

--- db/connection.js
+++ db/connection.js
@@ -1,18 +1,21 @@
 const { Pool } = require("pg");
+
 const ENV = process.env.NODE_ENV || "development";
+
+require("dotenv").config({
+  path: `${__dirname}/../.env.${ENV}`,
+});
 
 const config = {};
 
 if (ENV === "production") {
   config.connectionString = process.env.DATABASE_URL;
   config.max = 2;
+} else {
+  config.database = process.env.PGDATABASE;
 }
 
-require("dotenv").config({
-  path: `${__dirname}/../.env.${ENV}`,
-});
-
-if (!process.env.PGDATABASE) {
+if (!process.env.PGDATABASE && !process.env.DATABASE_URL) {
   throw new Error("PGDATABASE or DATABASE_URL not set");
 }
 
